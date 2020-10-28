0ced35c0cfdec8f904ece63e8c9f14369b407a67
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2020-10-28 08:59:14 +0200

    Implements basic navbar plus the initial React settings

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <Navbar />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="max-width">
+        <div className="logo">
+          <a href="/">
+            Vinifo<span>lio</span>
+          </a>
+        </div>
+        <ul class="menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/">Skills</a>
+          </li>
+          <li>
+            <a href="/">Team</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
