ff5364649446c730cf7a5439a14715f596778c2d
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2020-11-30 12:21:11 +0200

    Adds the correct favicon and implements Google Analytics for test's sake

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
@@ -8,8 +8,14 @@ import Skills from "./components/Skills";
 import Team from "./components/Team";
 import ScrollUpButton from "./components/ScrollUpButton";
 import Contact from "./components/Contact";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
   return (
     <React.Fragment>
       <ScrollUpButton />
